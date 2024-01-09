cdc3c5dbb394cd34213ba634ca0fa6e863695c91
Sohel-Developer <sohel.webdevoloper@gmail.com>
2024-01-09 19:42:06 +0600

    Top Navbar Design Complete

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import logo from '../public/asd1.png'
 import boll from '../public/soccer-ball 1.png'
 import Image from 'next/image'
 import Link from 'next/link'
-import { FaAd, FaAlignLeft, FaChartBar, FaFootballBall, FaFutbol, FaGlobe, FaGripHorizontal } from "react-icons/fa";
+import { FaAd, FaAlignLeft, FaBell, FaChartBar, FaChevronDown, FaFootballBall, FaFutbol, FaGlobe, FaGripHorizontal, FaRegEnvelope, FaRegHeart, FaUserCircle } from "react-icons/fa";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -29,8 +29,28 @@ export default function RootLayout({ children }) {
             <Link className='sideBarLink' href='/'><FaGlobe /> </Link>
           </nav>
         </div>
-        <div>
-          <div className='bg-red-200'> Navbar</div>
+        <div className='w-full'>
+          <div className=' h-24   bg-[#0D0D0D] text-white flex justify-between items-center'>
+            <nav className='space-x-7 ps-7'>
+              <Link className='text-xl' href="/">Matches</Link>
+              <Link className='text-xl' href="/">Staking</Link>
+              <Link className='text-xl' href="/">Rewords</Link>
+              <Link className='text-xl' href="/">Trade</Link>
+            </nav>
+            <h1 className='text-2xl'>The Premier collection</h1>
+            <div className='flex justify-between items-center '>
+              <div className='flex gap-7 text-2xl pr-10'>
+                <Link href='/'><FaBell /></Link>
+                <Link href='/'><FaRegEnvelope /></Link>
+                <Link href='/'><FaRegHeart /></Link>
+              </div>
+              <div className='flex items-center justify-center gap-5'>
+                <Link className='text-5xl ' href="/"> <FaUserCircle /></Link>
+                <span className='flex items-center justify-center pr-5'>Wallet ID <FaChevronDown /></span>
+              </div>
+            </div>
+          </div>
+
           {children}
         </div>
 
